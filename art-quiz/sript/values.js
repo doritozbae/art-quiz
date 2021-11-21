dc7130a4b72e799ca1be1c412e2ccfb1794de2f9
doritozbae <doritosbae@mail.ru>
2021-11-22 00:18:17 +0300

    feat: add values

--- art-quiz/sript/values.js
+++ art-quiz/sript/values.js
@@ -5,7 +5,7 @@ export const categoriesPage = document.getElementById('categories-page');
 
 export const settingsBtn = document.querySelector('.settings-btn');
 export const closeSettingsBtn = document.querySelector('.close-btn');
-
+export const saveBtn = document.getElementById('save-btn');
 export const timeSwitch = document.querySelector('.setting-time-switch');
 export const soundButton = document.querySelector('.sound-button');
 export const muteButton = document.querySelector('.mute-button');
@@ -23,8 +23,60 @@ export const pictureQuiz = document.querySelector('.quiz-page');
 export const quitAlert = document.querySelector('.quit-alert');
 export const cancelQuitBtn = document.querySelector('.cancel-quit-btn');
 export const quitBtn = document.querySelector('.quit-btn');
+export const quitAlertCloseBtn = document.querySelector('.quit-alert-close-btn');
+
+export const correctAlert = document.querySelector('.right-answer-alert');
+export const correctAlertNext = document.querySelector('.correct-alert-btn');
+export const wrongAlert = document.querySelector('.wrong-answer-alert');
+export const wrongAlertNext = document.querySelector('.wrong-alert-btn');
+
+export const greenBg = document.querySelector('.correct-answer-layout');
+export const redBg = document.querySelector('.incorrect-answer-layout');
+
+export const correctPicture = document.querySelector('.correct-picture-img');
+export const correctPictureName = document.querySelector('.correct-picture-name');
+export const correctAuthorName = document.querySelector('.correct-author');
+export const wrongPicture = document.querySelector('.wrong-picture-img');
+export const wrongPictureName = document.querySelector('.wrong-picture-name');
+export const wrongAuthorName = document.querySelector('.wrong-author');
+
 
 export const menuItemCategories = document.querySelector('.categories-page-container-menu-categories');
 export const menuItemHome = document.querySelector('.categories-page-container-menu-home');
 export const menuItemScore = document.querySelector('.categories-page-container-menu-score');
 
+export const pictureContainer = document.querySelector('.quiz-page-container-picture');
+export const pictureQuestion = document.querySelector('.picture-quiz-question');
+export const pictureAnswBtn1 = document.querySelector('.quiz-button-1');
+export const pictureAnswBtn2 = document.querySelector('.quiz-button-2');
+export const pictureAnswBtn3 = document.querySelector('.quiz-button-3');
+export const pictureAnswBtn4 = document.querySelector('.quiz-button-4');
+export const picturePlace = document.querySelector('.picture-quiz-image');
+export const pictureButton = document.querySelectorAll('.quiz-button');
+
+//categories 
+export const portrait = document.querySelector('.portrait');
+export const impressionism = document.querySelector('.impressionism');
+export const expressionism = document.querySelector('.expressionism');
+export const landscape = document.querySelector('.landscape');
+export const still = document.querySelector('.still');
+export const avantGarde = document.querySelector('.avant-garde');
+export const renaissance = document.querySelector('.renaissance');
+export const surrealism = document.querySelectorAll('.surrealism');
+export const kitsch = document.querySelector('.kitsch');
+export const mininalism = document.querySelector('.mininalism');
+export const interior = document.querySelector('.interior');
+export const nude = document.querySelectorAll('.nude');
+
+export const resultsContainer = document.querySelectorAll('.results-page-container-categories');
+
+export const place0 = document.querySelector('.result-image0');
+export const place1 = document.querySelector('.result-image1');
+export const place2 = document.querySelector('.result-image2');
+export const place3 = document.querySelector('.result-image3');
+export const place4 = document.querySelector('.result-image4');
+export const place5 = document.querySelector('.result-image5');
+export const place6 = document.querySelector('.result-image6');
+export const place7 = document.querySelector('.result-image7');
+export const place8 = document.querySelector('.result-image8');
+export const place9 = document.querySelector('.result-image9');
