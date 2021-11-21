a9c5e9944a38cda7e2b538ec6a142d8496685b43
doritozbae <doritosbae@mail.ru>
2021-11-22 00:17:52 +0300

    feat: add results generation

--- art-quiz/sript/picturequiz.js
+++ art-quiz/sript/picturequiz.js
@@ -1,17 +1,15 @@
-import {picturePlace, correctPicture, correctPictureName, correctAuthorName, wrongPicture, wrongPictureName, wrongAuthorName, greenBg, redBg, pictureContainer, pictureQuestion, pictureAnswBtn1, pictureAnswBtn2, pictureAnswBtn3, pictureAnswBtn4, correctAlert, wrongAlert, pictureButton} from "./values.js";
+import {picturePlace, place0, place1,place2,place3,place4,place5, place6, place7, place8, place9, portrait, resultsContainer, correctPicture, correctPictureName, correctAuthorName, wrongPicture, wrongPictureName, wrongAuthorName, greenBg, redBg, pictureContainer, pictureQuestion, pictureAnswBtn1, pictureAnswBtn2, pictureAnswBtn3, pictureAnswBtn4, correctAlert, wrongAlert, pictureButton} from "./values.js";
 import images from "./img.js";
-// export function questionImage() {
+import * as categories from './categories.js';
 
-  
-
-// }
 let imageIndex = 0;
+
 // let buttonArray = [pictureAnswBtn1, pictureAnswBtn2, pictureAnswBtn3, pictureAnswBtn4];
-let min = 0;
-let max = 3;
+// let min = 0;
+// let max = 3;
+let userCorrectAnswers = 0;
 
-
-export function showQuestion() {
+export function showQuestion() { 
    let question = images[imageIndex];
    let buttonArray = [pictureAnswBtn1, pictureAnswBtn2, pictureAnswBtn3, pictureAnswBtn4];
 
@@ -31,7 +29,7 @@ export function showQuestion() {
    let randomNumber = getRandomBtn.randomNum;
    let rightAnswer = buttonArray[randomNumber];
 
-   console.log(randomNumber)
+   // console.log(randomNumber)
 
    // deleting correct and leaving incorrect answers array
    buttonArray.splice(randomNumber, 1);
@@ -43,12 +41,8 @@ export function showQuestion() {
    buttonArray[1].innerHTML = images[imageIndex+2].author;
    buttonArray[2].innerHTML = images[imageIndex+3].author;
 
-
-   console.log(rightAnswer); 
-   console.log(buttonArray); 
-
-   // imageIndex++;
-   // console.log(picturePlace.src)
+   // console.log(rightAnswer); 
+   // console.log(buttonArray); 
 
    //generating alert content 
    correctPictureName.innerHTML = question.name;
@@ -59,68 +53,36 @@ export function showQuestion() {
    wrongAuthorName.innerHTML = question.author;
    wrongPicture.src = `./assets/img/img/${imageIndex}.jpg`;
 
-
-   Array.from(pictureButton).forEach(element => {
-      element.addEventListener('click', () => {
-         if(element.classList.contains('yes')) {
-            correctAlert.style.display="block";
-            greenBg.style.display="block";
-           
-         }
-         else {
-            redBg.style.display="block";
-            wrongAlert.style.display="block";
-            console.log('wrong')
-         }
-      });
-   });
+   //generate results page 
+   let resultImgPlace = [place0, place1,place2, place3, place4, place5, place6, place7, place8, place9]
+   resultImgPlace[imageIndex].src = `./assets/img/img/${imageIndex}.jpg`;
 
    imageIndex++;
 }
 
 
-// export function generateAnswers() {
-//    // let questionNum = showQuestion.questionNum; 
-//    let question = images[imageIndex];
-
-//    function getRandomBtn(min, max){
-//       min = Math.ceil(min);
-//       max = Math.floor(max);
-//       let random = Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-//       getRandomBtn.randomNum = random;
-//       console.log(random);
-//    }
-//    getRandomBtn(0,3);
-
-//    let randomNumber = getRandomBtn.randomNum;
-//    let rightAnswer = buttonArray[randomNumber];
-
-//    // incorrect answers array
-//    buttonArray.splice(randomNumber, 1);
-
-//    // generate answers
-//    rightAnswer.innerHTML = question.author;
-//    buttonArray[0].innerHTML = 'jjjj'
-//    buttonArray[1].innerHTML = 'err'
-//    buttonArray[2].innerHTML = 'ertesrd'
+Array.from(pictureButton).forEach(element => {
+   element.addEventListener('click', () => {
+      if(element.classList.contains('yes')) {
+         userCorrectAnswers++;
+         correctAlert.style.display="block";
+         greenBg.style.display="block";
+         element.classList.remove('yes');
+      }
+      else {
+         redBg.style.display="block";
+         wrongAlert.style.display="block";
+         // console.log('wrong');
+           Array.from(pictureButton).forEach(element => {
+           element.classList.remove('yes'); 
+           });
+      }   
+   }); 
+});
 
 
-
-//    console.log(rightAnswer); 
-//    console.log(buttonArray); 
-
-// }
-// function showImg() {
-//    var img = document.createElement('img');
-//    img.src = `./assets/img/img/${imageIndex}.jpg`;
-//    pictureContainer.appendChild(img);
-//    imageIndex++
-// } 
-
-
-// export function getRandomBtn(min, max){
-//    min = Math.ceil(min);
-//    max = Math.floor(max);
-//    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-//    console.log(randomNum);
-// }
+// portrait.addEventListener('click', () => {
+//    categories.openPictureQuiz();
+//    let imageIndex = 0;
+//    showQuestion();
+// })
